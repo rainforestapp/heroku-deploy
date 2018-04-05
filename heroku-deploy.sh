59df66068efad58b2a6a678d6c58d164e7302d6b
Jan Grodowski <jgrodowski@gmail.com>
2018-04-05 15:59:50 +0200

    check in other heroku scripts

---
+++ heroku-deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+APP_NAME="${1:-}"
+SLEEP="${2:-}"
+
+if [[ -z $APP_NAME ]]; then
+  echo "Usage: $0 <env>"
+  exit 1
+fi
+
+git push -f "git@heroku.com:$APP_NAME.git" "$CIRCLE_SHA1:master"
+heroku run rake db:migrate --app "$APP_NAME" --exit-code
+.circleci/heroku-restart.sh "$APP_NAME" "$SLEEP"
---
+++ heroku-restart.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+APP_NAME="$1"
+SLEEP="${2:-0}"
+
+if [[ -z $APP_NAME ]]; then
+  echo "Usage: $0 <env> [delay]"
+  exit 1
+fi
+
+for i in {1..5}; do
+    echo "Try #$i"
+    echo "Waiting ${SLEEP} seconds..."
+    sleep "$SLEEP"
+    if heroku restart -a "$APP_NAME"; then
+        break
+    fi
+    sleep 5
+done
